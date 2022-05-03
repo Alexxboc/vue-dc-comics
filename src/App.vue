ab392f85efb9480ea284d58f7f1292d47cdbabd2
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-03 14:16:39 +0200

    First commit:
    - Create new vue cli project
    
    - Define components
    
    - Create css reset rule

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    
+    
+  </div>
+</template>
+
+<script>
+// import  from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* Common rules */
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+img {
+  max-width: 100%;
+}
+
+/* Utilities */
+.container {
+  max-width: 1440px;
+}
+/* Header */
+
+/* Main */
+// Section Hero Image
+
+// Section Navigator
+
+// Section DC Info
+
+/* Footer */
+
+</style>
